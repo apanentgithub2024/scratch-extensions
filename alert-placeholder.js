5b4ec5249c36734ca03ff094de63dac2cae61eef
apanentgithub2024 <165437400+apanentgithub2024@users.noreply.github.com>
2024-04-13 15:52:32 -0400

    Create alert-placeholder.js

---
+++ alert-placeholder.js
@@ -0,0 +1,38 @@
+(function(Scratch) {
+  "use strict"
+	
+  if (!Scratch.extensions.unsandboxed) {
+		throw new Error("Alert extension must run unsandboxed for use")
+  }
+	
+  class Extension {
+		getInfo() {
+			return {
+				id: "alert",
+				name: "Alert",
+				color1: "#00FFFF",
+				color2: "#008888",
+				color3: "#002222",
+				blocks: [
+					{
+						opcode: "alert",
+						type: Scratch.BlockType.COMMAND,
+						blockText: "alert [A]",
+						arguments: {
+							A: {
+								argumentType: Scratch.ArgumentType.STRING,
+								defaultValue: "this text"
+							}
+						}
+					}
+				]
+			}
+		}
+
+		alert(args) {
+			alert(args.A)
+		}
+	}
+
+	Scratch.extensions.register(new Extension())
+})(Scratch)
